0abb55107af17bffee80ce2e1e5e2b02a20496b5
lanxiao <lanxiao_uvic@yahoo.ca>
2018-08-09 17:25:39 -0700

    try to test heroku

--- bookstore/views.py
+++ bookstore/views.py
@@ -7,7 +7,12 @@ from .permissions import IsAdminOrReadOnly
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import status
+import requests
 
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 class BookList(generics.ListCreateAPIView):
         queryset = Book.objects.all()
